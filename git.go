213adf6e656a17038098200b693c1b306ae747db
Mahendra Kalkura <mahendra@kalkura.com>
2017-02-24 18:51:52 +0530

    git.go

--- git.go
+++ git.go
@@ -34,7 +34,26 @@ func visit(path string, file_info os.FileInfo, err error) error {
 }
 
 func process(path string) {
+	git_status(path)
+	git_remote_update_and_git_status(path)
+}
+
+func git_status(path string) {
 	command := fmt.Sprintf("cd %s && /usr/bin/git status", path)
+	output_bytes, err := exec.Command("/bin/bash", "-c", command).Output()
+	if err != nil {
+		panic(err)
+	}
+	output_string := string(output_bytes)
+	output_string = strings.Replace(output_string, "\n", "", -1)
+	if strings.Contains(output_string, "nothing to commit, working tree clean") {
+		return
+	}
+	fmt.Println(path)
+}
+
+func git_remote_update_and_git_status(path string) {
+	command := fmt.Sprintf("cd %s && /usr/bin/git remote update && /usr/bin/git status", path)
 	output_bytes, err := exec.Command("/bin/bash", "-c", command).Output()
 	if err != nil {
 		panic(err)
