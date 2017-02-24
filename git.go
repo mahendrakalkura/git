702dabf9a4808b8290e3f6b19fb8c17ab229d143
Mahendra Kalkura <mahendra@kalkura.com>
2017-02-24 18:55:46 +0530

    git.go

--- git.go
+++ git.go
@@ -34,34 +34,28 @@ func visit(path string, file_info os.FileInfo, err error) error {
 }
 
 func process(path string) {
-	git_status(path)
-	git_remote_update_and_git_status(path)
-}
-
-func git_status(path string) {
-	command := fmt.Sprintf("cd %s && /usr/bin/git status", path)
-	output_bytes, err := exec.Command("/bin/bash", "-c", command).Output()
-	if err != nil {
-		panic(err)
-	}
-	output_string := string(output_bytes)
-	output_string = strings.Replace(output_string, "\n", "", -1)
-	if strings.Contains(output_string, "nothing to commit, working tree clean") {
-		return
-	}
-	fmt.Println(path)
-}
-
-func git_remote_update_and_git_status(path string) {
 	command := fmt.Sprintf("cd %s && /usr/bin/git remote update && /usr/bin/git status", path)
+
 	output_bytes, err := exec.Command("/bin/bash", "-c", command).Output()
 	if err != nil {
 		panic(err)
 	}
+
 	output_string := string(output_bytes)
 	output_string = strings.Replace(output_string, "\n", "", -1)
-	if strings.Contains(output_string, "nothing to commit, working tree clean") {
-		return
+
+	one := "Changes not staged for commit"
+	if strings.Contains(output_string, one) {
+		fmt.Printf("%s: %s\n", path, one)
 	}
-	fmt.Println(path)
+
+	two := "Your branch is ahead"
+	if strings.Contains(output_string, two) {
+		fmt.Printf("%s: %s\n", path, two)
+	}
+
+	three := "Your branch is behind"
+	if strings.Contains(output_string, three) {
+		fmt.Printf("%s: %s\n", path, three)
+	}
 }
