f7a6dc324cf61e89cb74b2bfc1944fa8ec75bf23
Mahendra Kalkura <mahendra@kalkura.com>
2017-03-31 19:03:59 +0530

    git.go

--- git.go
+++ git.go
@@ -10,7 +10,7 @@ import (
 	"sync"
 )
 
-var wait_group sync.WaitGroup
+var waitGroup sync.WaitGroup
 
 func main() {
 	directory := flag.String("directory", "", "")
@@ -19,10 +19,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	wait_group.Wait()
+	waitGroup.Wait()
 }
 
-func visit(path string, file_info os.FileInfo, err error) error {
+func visit(path string, fileInfo os.FileInfo, err error) error {
 	directories := strings.Split(path, "/")
 	length := len(directories) - 1
 	if directories[length] != ".git" {
@@ -34,60 +34,54 @@ func visit(path string, file_info os.FileInfo, err error) error {
 	if directories[length-2] == "deps" {
 		return nil
 	}
-	if strings.Contains(path, "github.com") && !strings.Contains(path, "mahendrakalkura") {
-		return nil
-	}
-	if strings.Contains(path, "golang.org") {
-		return nil
-	}
-	if strings.Contains(path, "gopkg.in") {
+	if !strings.Contains(path, "/bitbucket.org/") && !strings.Contains(path, "/github.com/mahendrakalkura.com") {
 		return nil
 	}
 	directories = directories[:length]
 	path = strings.Join(directories, "/")
-	wait_group.Add(1)
+	waitGroup.Add(1)
 	go process(path)
 	return nil
 }
 
 func process(path string) {
-	defer wait_group.Done()
+	defer waitGroup.Done()
 
 	command := fmt.Sprintf("cd %s && /usr/bin/git remote update && /usr/bin/git status", path)
 
-	output_bytes, err := exec.Command("/bin/bash", "-c", command).Output()
-	output_string := string(output_bytes)
-	output_string = strings.Replace(output_string, "\n", "", -1)
+	outputBytes, err := exec.Command("/bin/bash", "-c", command).Output()
+	outputString := string(outputBytes)
+	outputString = strings.Replace(outputString, "\n", "", -1)
 	if err != nil {
 		fmt.Println(command)
-		fmt.Println(output_string)
+		fmt.Println(outputString)
 		panic(err)
 	}
 
 	one := "Your branch is behind"
-	if strings.Contains(output_string, one) {
+	if strings.Contains(outputString, one) {
 		command := fmt.Sprintf("cd %s && /usr/bin/git pull", path)
 
-		output_bytes, err := exec.Command("/bin/bash", "-c", command).Output()
-		output_string := string(output_bytes)
-		output_string = strings.Replace(output_string, "\n", "", -1)
+		outputBytes, err := exec.Command("/bin/bash", "-c", command).Output()
+		outputString := string(outputBytes)
+		outputString = strings.Replace(outputString, "\n", "", -1)
 		if err != nil {
 			fmt.Println(command)
-			fmt.Println(output_string)
+			fmt.Println(outputString)
 			panic(err)
 		}
 
-		wait_group.Add(1)
+		waitGroup.Add(1)
 		go process(path)
 	}
 
 	two := "Your branch is ahead"
-	if strings.Contains(output_string, two) {
+	if strings.Contains(outputString, two) {
 		fmt.Printf("%29s: %s\n", two, path)
 	}
 
 	three := "Changes not staged for commit"
-	if strings.Contains(output_string, three) {
+	if strings.Contains(outputString, three) {
 		fmt.Printf("%29s: %s\n", three, path)
 	}
 }
